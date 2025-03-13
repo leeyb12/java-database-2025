23dbff378d1d4157d0f3a58a04e49869eb04a6b3
leeyb12 <dbsqja1233@naver.com>
2025-03-13 17:10:32 +0900

    3일차 마무리

--- Day03/sql03_조인기본.sql
+++ Day03/sql03_조인기본.sql
@@ -42,10 +42,11 @@ SELECT e.employee_id, e.first_name || ' ' || e.last_name AS "full_name"
    AND l.location_id = d.location_id
    AND d.department_id = 60;
 
+-- ANSI 표준은 오라클방식보다 복잡
 SELECT e.employee_id, e.first_name || ' ' || e.last_name AS "full_name"
      , e.email, e.phone_number 
      , e.hire_date, e.job_id
-     , d. department_id 
+     , d.department_id 
      , d.department_name 
      , l.city, l.STATE_PROVINCE, l.STREET_ADDRESS 
   FROM employees e
@@ -54,3 +55,58 @@ SELECT e.employee_id, e.first_name || ' ' || e.last_name AS "full_name"
  INNER JOIN locations l 
     ON l.location_id = d.location_id
  WHERE d.department_id = 60;
+
+-- 외부조인
+-- 보통 PK와 FK간의 일치하는 조건의 데이터를 찾는 것 - 내부조인
+-- PK와 FK간의 일치하지 않는 조건의 데이터도 찾는 것 - 외부조인
+
+-- 테이블1 OUTER JOIN 테이블2
+-- 테이블1번을 기준으로 외부조인 LEFT OUTER JOIN
+-- 테이블2번을 기준으로 외부조인 RIGHT OUTER JOIN
+
+SELECT *
+  FROM employees e
+ INNER JOIN departments d 
+    ON e.department_id = d.department_id
+ WHERE e.department_id IS NULL;
+
+-- ANSI 표준문법
+-- employees 테이블에는 있는데 departments 테이블에는 없는 데이터를 같이 출력해줘
+SELECT *
+  FROM employees e
+  LEFT OUTER JOIN departments d
+    ON e.department_id = d.department_id
+ WHERE e.department_id IS NULL;
+
+-- departments 테이블에는 있는데 employees 테이블에 없는 데이터를 같이 출력해줘
+SELECT *
+  FROM employees e
+ RIGHT OUTER JOIN departments d
+    ON e.department_id = d.department_id;
+
+-- Oracle 문법
+-- (+)만족하지 않는 조건도 더 나오게 한다는 뜻
+-- LEFT OUTER JOIN
+SELECT *
+  FROM employees e, departments d 
+ WHERE e.department_id = d.department_id(+);
+
+-- RIGHT OUTER JOIN
+SELECT *
+  FROM employees e, departments d
+ WHERE e.department_id(+) = d.department_id;
+
+-- INNER JOIN은 INNER를 생략가능
+-- OUTER JOIN에만 LEFT, RIGHT존재하므로 OUTER 생략가능
+
+-- 셀프조인: 자기자신을 두번 사용하는 조언
+SELECT e1.employee_id
+     , e1.first_name || ' ' || e1.last_name AS "full_emp_name"
+     , e1.job_id 
+     , e1.manager_id 
+     , e2.first_name || ' ' || e2.last_name AS "full_mng_name"
+     , e2.job_id 
+  FROM employees e1, employees e2
+ WHERE e1.manager_id = e2.employee_id(+)
+ ORDER BY e1.manager_id; 
+ 
