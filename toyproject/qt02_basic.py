87a1d2a5d6c3893f9d3a08372c8f6b7748ca11ea
leeyb12 <dbsqja1233@naver.com>
2025-03-19 15:20:10 +0900

    7일차 수정 커밋

--- toyproject/qt02_basic.py
+++ toyproject/qt02_basic.py
@@ -1,16 +1,16 @@
 ## PyQt5 첫 윈도우앱 개발
-import sys 
+import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-class DevWindow(QMainWindow):    
-    def __init__(self): 
+class DevWindow(QMainWindow):
+    def __init__(self):
         super().__init__()
 
         self.setWindowTitle('My First App') # 윈도우 타이틀 지정
-        self.setWindowIcon(QIcon('./image/databases.png')) # 윈도우 아이콘 지정
+        self.setWindowIcon(QIcon('./image/database.png')) # 윈도우 아이콘 지정
         # self.setGeometry(300, 200, 600, 350) # 윈도우 위치(x, y), 크기(w, h) 지정
-        self.resize(600, 350) # 윈도우 지정
+        self.resize(600, 350) # 윈도우 크기 지정
         
         # 윈도우 바탕화면 정중앙에 위치
         qr = self.frameGeometry() # 현재 만들어진 Qt앱 위치와 크기를 받아옴
@@ -19,7 +19,7 @@ class DevWindow(QMainWindow):
         self.move(qr.topLeft())
 
 
-app = QApplication(sys.argv)   
-win = DevWindow()   
-win.show()  
-app.exec() 
+app = QApplication(sys.argv)
+win = DevWindow()
+win.show()
+app.exec_()
